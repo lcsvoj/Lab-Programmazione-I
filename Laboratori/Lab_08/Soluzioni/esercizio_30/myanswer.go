8197ca9c78cb3b4218f370edea9b1fed449ece11
Lucas Vilela <lucas.vileladeoliveirajunior@studenti.unimi.it>
2023-12-14 11:20:29 +0100

    wrong version

---
+++ Laboratori/Lab_08/Soluzioni/esercizio_30/myanswer.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	inputString := os.Args[1]
+	primi := tagliaStringa(inputString)
+	for i := 0; i < len(primi); i++ {
+		fmt.Println("Risultato:", primi[i])
+	}
+}
+
+func Primo(n int) bool {
+	if n == 0 || n == 1 {
+		return false
+	}
+
+	for i := 2; i < n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func tagliaStringa(s string) (primi []int) {
+
+	for i := 0; i < len(s); i++ {
+		for j := 1; j <= 3; j++ {
+			pezzo := s[:i] + s[i+j:]
+			n, _ := strconv.Atoi(pezzo)
+			if Primo(n) {
+				primi = append(primi, n)
+			}
+		}
+	}
+
+	return primi
+}
