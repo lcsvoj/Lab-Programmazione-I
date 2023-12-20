8cba4fe08bc3c05f31e173cd566f2668656ba31d
Lucas Vilela <lucas.vileladeoliveirajunior@studenti.unimi.it>
2023-12-20 17:47:00 +0100

    L09.8 bugged

---
+++ Laboratori/Lab_09/Soluzioni/Mappe/esercizio_8/myanswer.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	// leggi i dati forniti
+	saldoIniziale, operazioni := LeggiTesto()
+
+	// formatta le date
+	listaDiDate := creaOrdine(operazioni)
+
+	// stampa il saldo iniziale
+	fmt.Printf("%-25s%11.2f\n\n", "SALDO INIZIALE:", saldoIniziale)
+
+	// stampa le operazione e calcola il saldo
+	stampaOperazioni(listaDiDate, operazioni)
+}
+
+func LeggiTesto() (float64, map[string][]string) {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	// salva il saldo iniziale
+	scanner.Scan()
+	saldoIniziale, _ := strconv.ParseFloat(scanner.Text(), 64)
+
+	// salva le operazioni
+	operazioni := make(map[string][]string)
+	for scanner.Scan() {
+		riga := strings.Split(scanner.Text(), ";") // riga = [data, tipo, importo]
+		operazioni[padronizzaData(riga[0])] = append(operazioni[padronizzaData(riga[0])], riga[1:]...)
+	}
+	return saldoIniziale, operazioni
+}
+
+func padronizzaData(data string) string {
+	// ritorna una stringa padronizatta YYYY-MM-DD
+
+	dataSlice := strings.Split(data, "/") // dataSlice = [anno, mese, giorno]
+
+	// inverti la data se neccessario
+	if len(dataSlice[0]) != 4 {
+		dataSlice[0], dataSlice[2] = dataSlice[2], dataSlice[0]
+	}
+
+	// mese e giorno devono essere stampati con 2 cifre
+	for i := 0; i < 3; i++ {
+		if len(dataSlice[i]) == 1 {
+			dataSlice[i] = "0" + dataSlice[i]
+		}
+	}
+
+	// rimonta la data con il formatto giusto
+	return fmt.Sprintf(dataSlice[0]) + "-" + dataSlice[1] + "-" + dataSlice[2]
+}
+
+func formattaData(data string) string {
+	// partendo da una stringa padronizzata YYYY-MM-DD, ritorna una stringa formattata come DD-MM-YYYY
+
+	dataSlice := strings.Split(data, "-") // dataSlice = [anno, mese, giorno]
+	return fmt.Sprintf(dataSlice[2]) + "-" + dataSlice[1] + "-" + dataSlice[0]
+}
+
+func creaOrdine(operazioni map[string][]string) (ordineDate []string) {
+	for data, _ := range operazioni {
+		ordineDate = append(ordineDate, data)
+		sort.Strings(ordineDate)
+	}
+	fmt.Println("\nORDINE DATE:", ordineDate)
+	return ordineDate
+}
+
+func stampaOperazioni(ordineDate []string, operazioni map[string][]string) {
+
+	for _, data := range ordineDate {
+		fmt.Println("DATA:", formattaData(data))
+
+		var saldoGiornaliero float64
+		for _, operazione := range operazioni[data] {
+			fmt.Println("Operazione:", operazione)
+			importo := float64(operazione[1])
+			if operazione[0] == 'P' {
+				fmt.Printf("%-25s%11.2f\n", "Prelievo:", importo)
+				saldoGiornaliero -= importo
+			} else {
+				fmt.Printf("%-25s%11.2f\n", "Versamento:", importo)
+				saldoGiornaliero += importo
+			}
+		}
+		fmt.Printf("%-25s%11s\n", "", strings.Repeat("_", 11))
+		fmt.Printf("%-25s%11.2f\n", "SALDO GIORNALIERO:", saldoGiornaliero)
+		fmt.Printf("%-25s%11s\n\n", "", strings.Repeat("=", 11))
+		saldoGiornaliero = 0
+	}
+}
