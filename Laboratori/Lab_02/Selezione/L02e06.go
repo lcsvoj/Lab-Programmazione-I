afe4da6ee3f54c9d82ef970ad533808a5efee177
Lucas Vilela <lucasvilela.jr@gmail.com>
2024-02-02 16:28:19 +0100

    L02e06 done

---
+++ Laboratori/Lab_02/Selezione/L02e06.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Print("Inserisci un intero: ")
+	fmt.Scan(&n)
+
+	if n%10 == 0 {
+		fmt.Println(n, "è multiplo di 10")
+	} else {
+		fmt.Println(n, "non è multiplo di 10")
+	}
+}
