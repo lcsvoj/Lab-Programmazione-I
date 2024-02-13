cdad7ad1c9ef6d71ed0f810406f443e3fdafa531
Lucas Vilela <lucasvilela.jr@gmail.com>
2024-02-13 14:34:21 +0100

    L10e07 e L10e08 done over L10e06

--- Laboratori/Lab_10/Strutture_e_puntatori/L10e06.go
+++ Laboratori/Lab_10/Strutture_e_puntatori/L10e06.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 type Frazione struct {
@@ -12,17 +14,28 @@ type Frazione struct {
 }
 
 func main() {
-	n, d := LeggiNumeri()
-	f := NuovaFrazione(n, d)
-	Riduci(f)
-	fmt.Println(String(*f))
+	frazioni := LeggiFrazioni()
+	moltiplicazione := MoltiplicaN(frazioni)
+	Riduci(moltiplicazione)
+	fmt.Println("Prodotto:", String(*moltiplicazione))
 }
 
-func LeggiNumeri() (n, d int) {
-	input := os.Args[1:]
-	n, _ = strconv.Atoi(input[0])
-	d, _ = strconv.Atoi(input[1])
-	return n, d
+func LeggiFrazioni() []Frazione {
+	fmt.Println("Inserisci numeratore e denominatore delle frazioni:")
+	scanner := bufio.NewScanner(os.Stdin)
+	frazioni := make([]Frazione, 0)
+	for scanner.Scan() {
+		riga := scanner.Text()
+		if riga == "" {
+			break
+		}
+		numeri := strings.Split(riga, " ")
+		numeratore, _ := strconv.Atoi(numeri[0])
+		denominatore, _ := strconv.Atoi(numeri[1])
+
+		frazioni = append(frazioni, *NuovaFrazione(numeratore, denominatore))
+	}
+	return frazioni
 }
 
 func NuovaFrazione(numeratore, denominatore int) *Frazione {
@@ -32,6 +45,22 @@ func NuovaFrazione(numeratore, denominatore int) *Frazione {
 
 func String(f Frazione) string {
 	return fmt.Sprintf("%d/%d", f.numeratore, f.denominatore)
+}
+
+func Moltiplica(f1, f2 Frazione) *Frazione {
+	f := Frazione{
+		numeratore:   f1.numeratore * f2.numeratore,
+		denominatore: f1.denominatore * f2.denominatore,
+	}
+	return &f
+}
+
+func MoltiplicaN(fN []Frazione) *Frazione {
+	risultato := Frazione{1, 1}
+	for _, f := range fN {
+		risultato = *Moltiplica(risultato, f)
+	}
+	return &risultato
 }
 
 func Riduci(f *Frazione) {
